5d287d70bc77f56a2aa4f5686f19ba5cdb5ca58b
kleitonalbuquerque <kleiton2102@gmail.com>
2022-02-18 11:53:27 -0300

    Requisições HTTP

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,28 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        let result = res.data.results;
+        result.push(this.pokemons);
+        console.log("pokemons", result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
 
