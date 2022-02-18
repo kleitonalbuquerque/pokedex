2d24c9f63118192ef120b3da7f9351f43b6d2897
kleitonalbuquerque <kleiton2102@gmail.com>
2022-02-18 13:51:44 -0300

    Buscando mais informações

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -6,11 +6,27 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      pokemon: {},
+    };
+  },
   props: {
     num: Number,
     name: String,
     url: String,
+  },
+  created: function () {
+    axios.get(this.url).then((res) => {
+      // console.log(res.data);
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+      console.log(this.pokemon);
+    });
   },
   filters: {
     upper: function (value) {
